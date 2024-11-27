19000eeaafb65a1c42dbe8b734a3c659cff4ea2c
NickTriance <114621135+NickTriance@users.noreply.github.com>
2024-11-27 11:48:29 -0800

    ui is passable now

--- ovigoaltracker/src/App.vue
+++ ovigoaltracker/src/App.vue
@@ -4,77 +4,41 @@ import Tracker from './components/Tracker.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div>
+    <header class="banner">
+    </header>
 
-    <div class="wrapper">
-      <Tracker />
-    </div>
-  </header>
-
-
+    <section class="tracker-section">
+      <div class="wrapper">
+        <Tracker />
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+* {
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.banner {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 35vh;
+  background-image: url('@/assets/header.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.tracker-section {
+  background-color: black;
+  color: white;
+  padding: 2rem;
+  min-height: 100vh;
 }
 </style>
